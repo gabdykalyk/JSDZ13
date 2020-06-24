b9cb7ee6ff2755ccbd79b824327f8e017cf62954
gabdykalyk <gabdykalyk@gmail.com>
2020-06-24 22:07:16 +0600

    TASK9 fixed

--- script.js
+++ script.js
@@ -77,10 +77,7 @@ console.log(access)
 let num1 = parseFloat(prompt("Введите первое число"))
 let num2 = parseFloat(prompt("Введите второе число"))
 let num3 = parseFloat(prompt("Введите третье число"))
-if (num1 === num2 || num1 === num3 || num2 === num3) {
-    console.log("Ошибка, есть одинаковые числа")
-}
-    else if (num1 > num2 && num1 > num3) {
+if (num1 > num2 && num1 > num3) {
     console.log(num1)
 } else if (num2 > num1 && num2 > num3) {
     console.log(num2)
@@ -89,28 +86,36 @@ if (num1 === num2 || num1 === num3 || num2 === num3) {
 }
 
 // TASK9
-
 num1 = parseFloat(prompt("Введите первое число"))
 num2 = parseFloat(prompt("Введите второе число"))
 num3 = parseFloat(prompt("Введите третье число"))
-if (num1 === num2 || num1 === num3 || num2 === num3) {
-    console.log("Ошибка, есть одинаковые числа")
-} else if (num1 > num2 && num1 > num3) {
-    console.log(num1)
+if (num1 > num2 && num1 > num3) {
+    if (num2 < num3) {
+        console.log(num1);
+        console.log(num2);
+    }
+    else {
+        console.log(num1);
+        console.log(num3);
+    }
 } else if (num2 > num1 && num2 > num3) {
-    console.log(num2)
-} else {
-    console.log(num3)
-}
-
-if (num1 === num2 || num1 === num3 || num2 === num3) {
-    console.log("Ошибка, есть одинаковые числа")
-} else if (num1 < num2 && num1 < num3) {
-    console.log(num1)
-} else if (num2 < num1 && num2 < num3) {
-    console.log(num2)
-} else {
-    console.log(num3)
+    if (num1 < num3) {
+        console.log(num2);
+        console.log(num1);
+    }
+    else {
+        console.log(num2);
+        console.log(num3);
+    }
+} else if (num3 > num1 && num3 > num2) {
+    if (num1 < num2) {
+        console.log(num3);
+        console.log(num1);
+    }
+    else {
+        console.log(num3);
+        console.log(num2);
+    }
 }
 
 // TASK10
